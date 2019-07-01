b5db31413ca0fccb43a150eecd4729cd48421192
Geislor <geislor@gmail.com>
2019-07-01 16:54:57 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='guaiaca',
+      version='0.1',
+      description='Simple integration with AWS S3',
+      url='https://github.com/geislor/guaiaca',
+      author='Geislor Crestani',
+      author_email='geislor@gmail.com',
+      license='MIT',
+      packages=['guaiaca'],
+      zip_safe=False)
