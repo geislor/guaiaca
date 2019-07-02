0ae7697ec9f9989110474b29f13532294b3b0ba4
Geislor <geislor@gmail.com>
2019-07-02 15:25:13 -0300

    fix version format

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='guaiaca',
-      version_format='{tag}.dev{commitcount}+{gitsha}',
+      version_format='{tag}.dev{commitcount}',
       setup_requires=['setuptools-git-version'],
       description='Simple integration with AWS S3',
       url='https://github.com/geislor/guaiaca',
